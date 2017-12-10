7915d76a08832b768b0463c759cc2fb69b712f18
Andy Barron <AndrewLBarron@gmail.com>
2017-12-09 17:46:47 -0800

    cancel parallelLimit on error

--- src/parallelLimit.ts
+++ src/parallelLimit.ts
@@ -1,23 +1,33 @@
 import { AsyncFunction } from './types';
 
 export = <T>(tasks: AsyncFunction<T>[], limit: number = Infinity): Promise<T[]> => {
-  const numThreads = Math.min(tasks.length, Math.round(limit));
+  const numThreads = Math.max(1, Math.min(tasks.length, limit));
+  const handle: CancelHandle = {};
   const configs = tasks.map((task, index) => ({task, index}));
   const results: T[] = [];
   const threadPromises: Promise<void>[] = [];
   for (let i = 0; i < numThreads; i++) {
-    threadPromises.push(startThread(configs, results));
+    threadPromises.push(startThread(configs, results, handle));
   }
-  return Promise.all(threadPromises).then(() => results);
+  return Promise.all(threadPromises)
+    .then(() => results)
+    .catch((error) => {
+      handle.canceled = true;
+      throw error;
+    });
 };
+
+interface CancelHandle {
+  canceled?: boolean,
+}
 
 interface TaskConfig<T> {
   task: AsyncFunction<T>,
   index: number,
 }
 
-const startThread = async <T> (taskConfigs: TaskConfig<T>[], results: T[]) => {
-  while (taskConfigs.length) {
+const startThread = async <T> (taskConfigs: TaskConfig<T>[], results: T[], handle: CancelHandle) => {
+  while (taskConfigs.length && !handle.canceled) {
     const {task, index} = taskConfigs.pop() as TaskConfig<T>;
     const result = await task();
     results[index] = result;
