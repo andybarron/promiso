b220639b800c527a30f1c4d3779d335754a49dba
Andy Barron <AndrewLBarron@gmail.com>
2017-12-11 02:36:22 -0800

    linter fix

--- src/concatLimit.ts
+++ src/concatLimit.ts
@@ -3,6 +3,6 @@ import { AsyncFunction, Collection } from './types';
 import { flatten } from './utils';
 
 export default <A, B> (items: Collection<A>, limit: number,
-                 f: AsyncFunction<A, Array<B>>): Promise<Array<B>> => {
+                       f: AsyncFunction<A, Array<B>>): Promise<Array<B>> => {
   return mapLimit(items, limit, f).then(flatten);
 };
--- src/mapLimit.ts
+++ src/mapLimit.ts
@@ -3,7 +3,7 @@ import { AsyncFunction, AsyncSupplier, Collection } from './types';
 import { mapCollection } from './utils';
 
 export default <A, B> (items: Collection<A>, limit: number,
-                 f: AsyncFunction<A, B>): Promise<Array<B>> => {
+                       f: AsyncFunction<A, B>): Promise<Array<B>> => {
   const tasks: Array<AsyncSupplier<B>> = mapCollection(items, (item) => () => f(item));
   return parallelLimit(tasks, limit);
 };
